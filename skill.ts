267f17cfc0bdd24cb3e4e312ece807ff7b0691eb
Tim Sparg <6872586+timothysparg@users.noreply.github.com>
2021-08-12 21:49:35 +0200

    revert to container skill

--- skill.ts
+++ skill.ts
@@ -40,7 +40,7 @@ export const Skill = skill<FormatterConfiguration & { repos: any }>({
 
 	containers: {
 		formatter: {
-			image: "docker.io/kludgeworks/intellij-formatter-skill:1.0.19",
+			image: "docker.io/kludgeworks/intellij-formatter-skill",
 			resources: {
 				limit: {
 					cpu: 2,
